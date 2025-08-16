e6c40c8f76f07d83ff72558fa7cdad33fc432451
neeta <neeta.rani@mail.vinove.com>
2025-08-16 20:08:10 +0530

    sql

---
+++ SQLDatabase/InterviewQueries.sql
@@ -0,0 +1,32 @@
+﻿
+ --Find the second highest salary
+Select max(salary) as SecondHighestSalary from Employee
+ where salary < (select max(salary) from Employee);
+
+
+ --Usng CTE to find the second highest salary
+ SELECT Salary
+FROM (
+    SELECT Salary, ROW_NUMBER() OVER (ORDER BY Salary DESC) AS rn
+    FROM Employee
+) t
+WHERE rn = 2;
+ 
+ -- Find employees who earn the highest salary in each department
+SELECT e.Name, e.Salary, d.DepartmentName
+FROM Employee e
+JOIN Department d ON e.DepartmentId = d.DepartmentId
+WHERE e.Salary = (
+    SELECT MAX(Salary) 
+    FROM Employee 
+    WHERE DepartmentId = e.DepartmentId
+);
+
+
+--Find duplicate employee names
+ 
+	SELECT Name, COUNT(*) AS Count
+	FROM Employee
+	GROUP BY Name
+	HAVING COUNT(*) > 1
+ 
---
+++ SQLDatabase/TableSchema.sql
@@ -0,0 +1,40 @@
+﻿GO
+CREATE TABLE Department (
+    DepartmentId INT PRIMARY KEY,
+    DepartmentName VARCHAR(100) NOT NULL
+);
+
+
+CREATE TABLE Employee (
+    EmployeeId INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Salary DECIMAL(10,2),
+    DepartmentId INT,
+    FOREIGN KEY (DepartmentId) REFERENCES Department(DepartmentId)
+);
+
+
+INSERT INTO Department (DepartmentId, DepartmentName) VALUES
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance');
+
+INSERT INTO Employee (EmployeeId, Name, Salary, DepartmentId) VALUES
+(101, 'Alice', 6000, 1),
+(102, 'Bob', 8000, 2),
+(103, 'Charlie', 5000, 2),
+(104, 'David', 7500, 3),
+(105, 'Eve', 9000, 2);
+
+SELECT * FROM Employee;
+
+----Get employees with salary greater than 7000
+--SELECT Name, Salary
+--FROM Employee
+--WHERE Salary > 7000;
+
+
+----Find employees with their department names (JOIN)
+--SELECT e.Name, e.Salary, d.DepartmentName
+--FROM Employee e
+--JOIN Department d ON e.DepartmentId = d.DepartmentId;
