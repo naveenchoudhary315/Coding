2dc624709dfbf8decc27c24070a43d2e17e716eb
Naveen <naveen.choudhary315@gmail.com>
2026-05-02 10:25:52 +0530

    fixed compile issues

---
+++ WebAPI/Controllers/CusotomController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class CusotomController : Controller
+    {
+        // Add basic api method and versioning cases.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
