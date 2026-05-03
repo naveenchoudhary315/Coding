c98d4938eb7cfc7e74fd4e79a5285aedc4e2cca4
Naveen <naveen.choudhary315@gmail.com>
2026-05-03 10:38:14 +0530

    options pattern.

---
+++ OptionsPattern/MySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OptionsPattern
+{
+    public class MySettings
+    {
+        public string AppName { get; set; }
+        public int MaxUsers { get; set; }
+    }
+}
---
+++ OptionsPattern/Service.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace OptionsPattern
+{
+    public class Service
+    {
+        private readonly MySettings _settings;
+       
+        public Service(IOptions<MySettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_settings.AppName);
+            Console.WriteLine(_settings.MaxUsers);
+        }
+    }
+}
